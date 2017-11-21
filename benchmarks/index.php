32a46e40ebf326af2f48c2f107bbc8288927b634
Nyrox <mark.oliver@hotmail.de>
2017-11-21 13:03:25 +0100

    Did some shit

---
+++ benchmarks/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+		
+		<style media="screen">
+			span {
+				display: block;
+				width: 32px;
+				height: 32px;
+				background: red;
+				transition: all 400ms ease;
+				margin-bottom: 2px;
+				
+			}
+			span.foo {
+				width: 64px;
+				background: blue;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="container query-perf-test">
+				<span class="span"></span>
+				<span class="span"></span>
+				<span class="span"></span>
+				<span class="span"></span>
+				<span class="span"></span>
+				<span class="span"></span>
+				<span class="span"></span>
+				<span class="span"></span>
+				<span class="span"></span>
+				<span class="span"></span>
+				<span class="span"></span>
+				<span class="span"></span>
+				<span class="span"></span>
+				<span class="span"></span>
+				<span class="span"></span>
+		</div>
+		
+		<script
+			  src="https://code.jquery.com/jquery-3.2.1.min.js"
+			  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+			  crossorigin="anonymous"></script>
+			  
+		<script src="main.js">
+		</script>
+		<script type="text/javascript">
+			function bench(id, iterations, b) {
+				var now = performance.now();
+				for(var i = 0; i < iterations; i++) {
+					b();
+				}
+				console.log((performance.now() - now).toFixed(2) + "ms : " + id);
+			}
+			var iterations = 1000000;
+			function bench_queries(iterations) {
+				bench("Doc class.class", iterations, function() { doc.class("container").class("span"); });
+				bench("Doc query", iterations, function() { doc.query(".container .span"); });
+				bench("jQ find", iterations, function() { $(".container").find(".span"); });
+				bench("jQ query", iterations, function() { $(".container .span"); });
+			}
+			
+			bench_queries(iterations);
+		</script>
+	</body>
+</html>
--- main.js
+++ main.js
@@ -12,14 +12,32 @@ doc.element = function(_elem) {
 };
 
 doc.element_list = function(_list) {
+	_list = Array.from(_list);
 
     this.each = function(_cb) {
         var index = 0;
-        Array.prototype.forEach.call(_list, function(elem) {
-            _cb(index, new doc.element(elem));
-            index++;
-        })
+        // Array.prototype.forEach.call(_list, function(elem) {
+        //     _cb(index, new doc.element(elem));
+        //     index++;
+        // })
+		for(; index < _list.length; index++) {
+			_cb(index, new doc.element(_list[index]));
+		}
     }
+	
+	this.class = function(_class) {
+		var _newList = [];
+		
+		for(var i = 0; i < _list.length; i++) {
+			var f = _list[i].getElementsByClassName(_class);
+			for(var b = 0; b < f.length; b++) {
+				_newList.push(f.item(b));
+			}
+		}
+		
+		_list = _newList;
+		return this;
+	}
 }
 
 doc.id = function(_id) {
@@ -28,4 +46,8 @@ doc.id = function(_id) {
 
 doc.class = function(_class) {
     return new doc.element_list(document.getElementsByClassName(_class));
+}
+
+doc.query = function(_query) {
+	return new doc.element_list(document.querySelectorAll(_query));
 }
